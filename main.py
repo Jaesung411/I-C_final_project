79410edaf0d4e1e712ab4f25418271456ae31997
jaesung <gitl@example.com>
2025-01-26 17:19:19 +0900

    add paging function

--- main.py
+++ main.py
@@ -16,4 +16,69 @@ def main():
     products = ProductDao().get_all_products()
     clients = clientDao().get_all_clients()
     categories = categoryDao().get_all_categories()
-    return render_template('home.html', products=products, int=int,clients=clients,categories=categories)
+
+    # 페이지네이션
+    page = int(request.args.get('page', 1))
+    per_page = 8
+    start = (page - 1) * per_page
+    end = start + per_page
+    total_pages = (len(products) + per_page - 1) // per_page
+    current_products = products[start:end]
+    pages = get_pagination(page, total_pages)
+    
+    return render_template('home.html', 
+                            products=current_products, 
+                            int=int,
+                            clients=clients,
+                            categories=categories,
+                            page=page, 
+                            pages=pages, 
+                            total_pages=total_pages
+                            )
+@blueprint.route('/search')    
+def search():
+    query = request.args.get('query', '')
+    products = ProductDao().get_all_products()
+    clients = clientDao().get_all_clients()
+    categories = categoryDao().get_all_categories()
+    if query:
+        print(1)
+        # images = imageDAO().search_images_by_query(session['userInfo']['userId'], query)
+    else:
+        products = ProductDao().get_all_products()
+
+    # 페이지네이션
+    page = request.args.get('page', 1, type=int)
+    per_page = 8
+    total_pages = (len(products) - 1) // per_page + 1
+    paginated_list = products[(page - 1) * per_page: page * per_page]
+    pages = get_pagination(page, total_pages)
+
+    return render_template('home.html', 
+                            clients=clients,
+                            categories=categories,
+                            products=paginated_list , 
+                            page=page, 
+                            pages=pages, 
+                            total_pages=total_pages,
+                            query=query,
+                            int=int)
+
+def get_pagination(page, total_pages, max_visible=10):
+    if total_pages <= max_visible:
+        return list(range(1, total_pages + 1))
+
+    visible_pages = []
+    visible_pages.append(1)
+    if page > 3:
+        visible_pages.append('...')
+
+    start = max(2, page - 1)
+    end = min(total_pages - 1, page + 1)
+    visible_pages.extend(range(start, end + 1))
+
+    if page < total_pages - 2:
+        visible_pages.append('...')
+        visible_pages.append(total_pages)
+
+    return visible_pages
